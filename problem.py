04beaff061a45242fcdba29d77744514fd04caee
vsmmlla <buscape@DESKTOP-CF1VISF.localdomain>
2020-03-10 16:10:26 +0100

    first commit

---
+++ problem.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+class Pb_edt:
+
+    def __init__(self, slots, profs, classes):
+        self.slots = slots
+        self.profs = profs
+        self.classes = classes
+
+    def nb_collision(self):
+        pass
+
+    def get_voisins(self, s):
+        pass
+
+    def print_solution(self, s):
+        pass
+
+
+if __name__ == '__main__':
+    K, M, N = 20, 32, 16
+    pb = Pb_edt(K, M, N)
+
+
